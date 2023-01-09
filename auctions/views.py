889b9e7e972d468206773324c2ae1bfb8437bac3
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2023-01-09 12:03:00 +0900

    Show active listings on index page

--- auctions/views.py
+++ auctions/views.py
@@ -8,7 +8,10 @@ from .models import User, Category, Listing
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+   activeListings = Listing.objects.filter(isActive=True)
+   return render(request, "auctions/index.html", {
+      "listings": activeListings
+    })
 
 def createListing(request):
    if request.method == "GET":
