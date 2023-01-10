66fed29477bd6ebc0ea212c96a3e32b11ea7c432
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2023-01-10 13:51:08 +0900

    Add selector to index page for categories

--- auctions/views.py
+++ auctions/views.py
@@ -9,8 +9,10 @@ from .models import User, Category, Listing
 
 def index(request):
    activeListings = Listing.objects.filter(isActive=True)
+   allCategories = Category.objects.all()
    return render(request, "auctions/index.html", {
-      "listings": activeListings
+      "listings": activeListings,
+      "categories": allCategories
     })
 
 def createListing(request):
